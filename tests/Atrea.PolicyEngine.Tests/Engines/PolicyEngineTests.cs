8bfcbdd5571f112974fead6365318d3d4d9419b4
Itabaiyu <45316999+itabaiyu@users.noreply.github.com>
2020-05-31 18:58:36 -0400

    Coverlet Output and PolicyEngine Unit Tests (#5)

---
+++ tests/Atrea.PolicyEngine.Tests/Engines/PolicyEngineTests.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections.Generic;
+using Atrea.PolicyEngine.Builders;
+using Atrea.PolicyEngine.Policies.Input;
+using Atrea.PolicyEngine.Policies.Output;
+using Atrea.PolicyEngine.Processors;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Atrea.PolicyEngine.Tests.Engines
+{
+    [TestFixture]
+    public class PolicyEngineTests
+    {
+        private const int Item = 1;
+
+        private IInputPolicy<int> _mockInputPolicyA;
+        private IInputPolicy<int> _mockInputPolicyB;
+        private IProcessor<int> _mockProcessor;
+        private IOutputPolicy<int> _mockOutputPolicy;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockInputPolicyA = Substitute.For<IInputPolicy<int>>();
+            _mockInputPolicyB = Substitute.For<IInputPolicy<int>>();
+            _mockProcessor = Substitute.For<IProcessor<int>>();
+            _mockOutputPolicy = Substitute.For<IOutputPolicy<int>>();
+        }
+
+        [Test]
+        public void Policy_Engine_Runs_Expected_Components_A()
+        {
+            _mockInputPolicyA.ShouldProcess(Item).Returns(InputPolicyResult.Continue);
+            _mockInputPolicyB.ShouldProcess(Item).Returns(InputPolicyResult.Continue);
+
+            var policyEngine = PolicyEngineBuilder<int>
+                .Configure()
+                .WithInputPolicies(new List<IInputPolicy<int>> { _mockInputPolicyA, _mockInputPolicyB })
+                .WithProcessors(new List<IProcessor<int>> { _mockProcessor })
+                .WithOutputPolicies(new List<IOutputPolicy<int>> { _mockOutputPolicy })
+                .Build();
+
+            policyEngine.Process(Item);
+
+            Received.InOrder(() =>
+            {
+                _mockInputPolicyA.ShouldProcess(Item);
+                _mockInputPolicyB.ShouldProcess(Item);
+                _mockProcessor.Process(Item);
+                _mockOutputPolicy.Apply(Item);
+            });
+        }
+
+        [Test]
+        public void Policy_Engine_Runs_Expected_Components_B()
+        {
+            _mockInputPolicyA.ShouldProcess(Item).Returns(InputPolicyResult.Continue);
+            _mockInputPolicyB.ShouldProcess(Item).Returns(InputPolicyResult.Accept);
+
+            var policyEngine = PolicyEngineBuilder<int>
+                .Configure()
+                .WithInputPolicies(new List<IInputPolicy<int>> { _mockInputPolicyA, _mockInputPolicyB })
+                .WithProcessors(new List<IProcessor<int>> { _mockProcessor })
+                .WithOutputPolicies(new List<IOutputPolicy<int>> { _mockOutputPolicy })
+                .Build();
+
+            policyEngine.Process(Item);
+
+            Received.InOrder(() =>
+            {
+                _mockInputPolicyA.ShouldProcess(Item);
+                _mockInputPolicyB.ShouldProcess(Item);
+                _mockProcessor.Process(Item);
+                _mockOutputPolicy.Apply(Item);
+            });
+        }
+
+        [Test]
+        public void Policy_Engine_Runs_Expected_Components_C()
+        {
+            _mockInputPolicyA.ShouldProcess(Item).Returns(InputPolicyResult.Accept);
+            _mockInputPolicyB.ShouldProcess(Item).Returns(InputPolicyResult.Reject);
+
+            var policyEngine = PolicyEngineBuilder<int>
+                .Configure()
+                .WithInputPolicies(new List<IInputPolicy<int>> { _mockInputPolicyA, _mockInputPolicyB })
+                .WithProcessors(new List<IProcessor<int>> { _mockProcessor })
+                .WithOutputPolicies(new List<IOutputPolicy<int>> { _mockOutputPolicy })
+                .Build();
+
+            policyEngine.Process(Item);
+
+            Received.InOrder(() =>
+            {
+                _mockInputPolicyA.ShouldProcess(Item);
+                _mockProcessor.Process(Item);
+                _mockOutputPolicy.Apply(Item);
+            });
+
+            _mockInputPolicyB.DidNotReceiveWithAnyArgs().ShouldProcess(Arg.Any<int>());
+        }
+
+        [Test]
+        public void Policy_Engine_Runs_Expected_Components_D()
+        {
+            _mockInputPolicyA.ShouldProcess(Item).Returns(InputPolicyResult.Reject);
+            _mockInputPolicyB.ShouldProcess(Item).Returns(InputPolicyResult.Continue);
+
+            var policyEngine = PolicyEngineBuilder<int>
+                .Configure()
+                .WithInputPolicies(new List<IInputPolicy<int>> { _mockInputPolicyA, _mockInputPolicyB })
+                .WithProcessors(new List<IProcessor<int>> { _mockProcessor })
+                .WithOutputPolicies(new List<IOutputPolicy<int>> { _mockOutputPolicy })
+                .Build();
+
+            policyEngine.Process(Item);
+
+            Received.InOrder(() => { _mockInputPolicyA.ShouldProcess(Item); });
+
+            _mockInputPolicyB.DidNotReceiveWithAnyArgs().ShouldProcess(Arg.Any<int>());
+            _mockProcessor.DidNotReceiveWithAnyArgs().Process(Arg.Any<int>());
+            _mockOutputPolicy.DidNotReceiveWithAnyArgs().Apply(Arg.Any<int>());
+        }
+
+        [Test]
+        public void Policy_Engine_Runs_Expected_Components_E()
+        {
+            _mockInputPolicyA.ShouldProcess(Item).Returns(InputPolicyResult.Continue);
+            _mockInputPolicyB.ShouldProcess(Item).Returns(InputPolicyResult.Reject);
+
+            var policyEngine = PolicyEngineBuilder<int>
+                .Configure()
+                .WithInputPolicies(new List<IInputPolicy<int>> { _mockInputPolicyA, _mockInputPolicyB })
+                .WithProcessors(new List<IProcessor<int>> { _mockProcessor })
+                .WithOutputPolicies(new List<IOutputPolicy<int>> { _mockOutputPolicy })
+                .Build();
+
+            policyEngine.Process(Item);
+
+            Received.InOrder(() =>
+            {
+                _mockInputPolicyA.ShouldProcess(Item);
+                _mockInputPolicyB.ShouldProcess(Item);
+            });
+
+            _mockProcessor.DidNotReceiveWithAnyArgs().Process(Arg.Any<int>());
+            _mockOutputPolicy.DidNotReceiveWithAnyArgs().Apply(Arg.Any<int>());
+        }
+    }
+}
